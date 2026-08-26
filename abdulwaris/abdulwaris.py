cc20fdc93ef1af8b33b615eed7340b0d8e85aee5
Âolowo waris <olowoabdulwaris@gmail.com>
2026-08-26 14:09:41 +0100

    git_repo

---
+++ abdulwaris/abdulwaris.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+from math import gamma
+
+df = pd.series ([15, 18, 19, 20, 20, 20, 21, 23, 23, 24, 24, 25, 45, 30, 33, 37, 21, 38, 42, 49, 50])
+print(df)git

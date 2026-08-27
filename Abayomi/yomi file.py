efb5d8f366aa7078cd5618b8ffaa99faa6e05490
hoyes-dst69 <you@labayomi2020@gmail.com>
2026-08-27 20:58:52 +0100

    second commit

--- Abayomi/yomi file.py
+++ Abayomi/yomi file.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 df =pd.Series([15, 18, 19, 20, 20, 20, 21, 23, 23, 24, 24, 25, 45, 30, 33, 37, 21, 38, 42, 49, 50])
 print(df)
@@ -17,3 +18,42 @@ print(f'variance: {Variance:,.2f}')
 
 std=df.std()
 print(f'standard deviation: {std:,.2f}')
+
+#generate random matrix A and b
+#add matrix a and b
+#multiply matrix a and b
+#tranpose of a and b
+#scalar multiplication by 5
+A = np.random.randint(low=1, high=10, size=(2,3))
+print('matrix A:')
+print(A)
+
+B = np.random.randint(low=1, high=10, size=(2,3))
+print('matrix B:')
+print(B)
+
+#Addition
+Sum = A + B
+print(f'sum of Matrix A and Matrix B:')
+print(Sum)
+
+#product
+Product = A * B
+print(f'Product of Matrix A and Matrix B:')
+print(Product)
+
+Transpose = A.T
+print(f'Transpose of Matrix A')
+print(Transpose)
+
+Transpose = B.T
+print(f'Transpose of Matrix B')
+print(Transpose)
+
+ScalarMultiplication = 5 * A
+print(f'Scalar Multiplication of Matrix A and a product of 5')
+print(ScalarMultiplication)
+
+ScalarMultiplication = 5 * B
+print(f'Scalar Multiplication of Matrix B and a product of 5')
+print(ScalarMultiplication)
